cad6a1ad9b95d6afdfac31302a56cb72ab5ee3bc
geejaywoo <geejaywoo@gmail.com>
2023-04-01 09:32:24 +0800

    init #comment [feat] delete useless

--- src/main/java/com/example/scaffold/repository/SchoolRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.scaffold.repository;
-
-import com.example.scaffold.model.jpa.oneToMany.School;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SchoolRepository extends JpaRepository<School, Integer> {
-}
--- src/main/java/com/example/scaffold/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.scaffold.repository;
-
-import com.example.scaffold.model.jpa.oneToMany.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-}
